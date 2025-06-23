58dcd0fdf7388b4365b249f958d36483b8f564a7
M-Saleh-N <salehnasser65784@gmail.com>
2025-06-23 22:25:08 +0300

    added an input

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,14 @@
 function AddTask({ newTask, setNewTask, addTask}){
     return(
         <div>
-            
+            <input
+            type="text"
+            value={newTask}
+            onChange={(e) =>
+                setNewTask(e.target.value)
+                }
+                placeholder="Add new task"
+            />
         </div>
 
     );
